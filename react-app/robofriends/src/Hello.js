403fcea030c33c62b7cf3839e12983762acbcd56
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-03 18:49:58 -0400

    adding Hello.css

--- react-app/robofriends/src/Hello.js
+++ react-app/robofriends/src/Hello.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import "./Hello.css";
 
 class Hello extends Component {
   render() {
-    return <h1>Hello World !!!</h1>;
+    return (
+      <div>
+        <h1>Hello World !!!</h1>
+        <p>Welcome to React</p>
+      </div>
+    );
   }
 }
 

8b4b4a8831e66cd9665150b221174aac99201457
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-04 12:41:00 -0400

    Updates

---
+++ precache-manifest.05203614cfc25ac50fc29db8199c5e1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/projects/robofriends-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e5dc550b837857f8d1ef",
+    "url": "/projects/robofriends-app/static/js/runtime~main.e5dc550b.js"
+  },
+  {
+    "revision": "248217f87213f6ab59a6",
+    "url": "/projects/robofriends-app/static/js/main.248217f8.chunk.js"
+  },
+  {
+    "revision": "d383a1e1c77c2d624205",
+    "url": "/projects/robofriends-app/static/js/1.d383a1e1.chunk.js"
+  },
+  {
+    "revision": "248217f87213f6ab59a6",
+    "url": "/projects/robofriends-app/static/css/main.1e11cff6.chunk.css"
+  },
+  {
+    "revision": "d383a1e1c77c2d624205",
+    "url": "/projects/robofriends-app/static/css/1.9bf828f0.chunk.css"
+  },
+  {
+    "revision": "4c93a017ba0ceb41b7225c668405db32",
+    "url": "/projects/robofriends-app/index.html"
+  }
+];

1e61d28e263244279988f9bd9de75ed3b048cda9
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-03 22:25:54 -0400

    styling the card

--- react-app/robofriends/src/Card.js
+++ react-app/robofriends/src/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div>
-      <img src="" alt="photo" />
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src="https://robohash.org/test?200x200" alt="photo" />
       <div>
         <h2>Jane Doe</h2>
         <p>jane.doe@gmail.com</p>

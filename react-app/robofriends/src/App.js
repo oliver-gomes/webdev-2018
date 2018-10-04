dbfb472ab2754f7acfe1f70bf030421df5d37780
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-04 01:58:47 -0400

    fetch from api

--- react-app/robofriends/src/App.js
+++ react-app/robofriends/src/App.js
@@ -8,9 +8,15 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ""
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   onSearchChange = event => {
@@ -24,13 +30,17 @@ class App extends Component {
         .includes(this.state.searchfield.toLowerCase());
     });
 
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />;
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1 className="f1 tc">Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />;
+        </div>
+      );
+    }
   }
 }
 

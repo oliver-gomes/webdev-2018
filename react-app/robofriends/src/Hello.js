b88a1e91e67816b3cee5174ad3cbee6171cfae8c
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-03 21:58:58 -0400

    update text

--- react-app/robofriends/src/Hello.js
+++ react-app/robofriends/src/Hello.js
@@ -5,7 +5,7 @@ class Hello extends Component {
   render() {
     return (
       <div className="f1 tc">
-        <h1>Hello Wasdasdasdld !!!</h1>
+        <h1>Hello World !!!</h1>
         <p>Welcome to React</p>
       </div>
     );

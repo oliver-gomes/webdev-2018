7c91827694f9d047b4196fe65c13a5a275a1a8e7
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-04 13:10:59 -0400

    Updates

---
+++ precache-manifest.32099ee719d015e6d6ef82611d8dc3ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/webdev-2018/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5a534ec7cd9f34471d93",
+    "url": "/webdev-2018/static/js/runtime~main.5a534ec7.js"
+  },
+  {
+    "revision": "2f50a7de0b850449e204",
+    "url": "/webdev-2018/static/js/main.2f50a7de.chunk.js"
+  },
+  {
+    "revision": "d383a1e1c77c2d624205",
+    "url": "/webdev-2018/static/js/1.d383a1e1.chunk.js"
+  },
+  {
+    "revision": "2f50a7de0b850449e204",
+    "url": "/webdev-2018/static/css/main.8fe18637.chunk.css"
+  },
+  {
+    "revision": "d383a1e1c77c2d624205",
+    "url": "/webdev-2018/static/css/1.9bf828f0.chunk.css"
+  },
+  {
+    "revision": "0b27afad2ee073c7080d8b6e80af24bd",
+    "url": "/webdev-2018/index.html"
+  }
+];

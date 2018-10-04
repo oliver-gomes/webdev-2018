9356607469f4ef23c7e58174d503f271d98bef70
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-03 21:56:34 -0400

    install  and implement tachyons

--- react-app/robofriends/src/Hello.js
+++ react-app/robofriends/src/Hello.js
@@ -4,8 +4,8 @@ import "./Hello.css";
 class Hello extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World !!!</h1>
+      <div className="f1 tc">
+        <h1>Hello Wasdasdasdld !!!</h1>
         <p>Welcome to React</p>
       </div>
     );

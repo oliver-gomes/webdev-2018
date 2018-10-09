88e77c646a928c9f9d23aff1f7897670bc74b381
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-08 23:28:08 -0400

    Navigation added

--- react-app/face-recognition-brain/src/container/App.js
+++ react-app/face-recognition-brain/src/container/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navigation from "../components/Navigation";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+
         {/* <Logo />
         <ImageLinkForm />
         <FaceRecognition /> */}

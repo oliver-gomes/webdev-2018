1552b0accceca5cb7774290234689759a54d74cd
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-09 00:00:58 -0400

    Logo import

--- react-app/face-recognition-brain/src/container/App.js
+++ react-app/face-recognition-brain/src/container/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-
-        {/* <Logo />
+        <Logo />
+        {/* 
         <ImageLinkForm />
         <FaceRecognition /> */}
       </div>

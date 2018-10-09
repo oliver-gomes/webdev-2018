ac15c6d6376a0626932be41b02cf6466973d2cc1
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-09 14:49:03 -0400

    calculating border box values

--- react-app/face-recognition-brain/src/container/App.js
+++ react-app/face-recognition-brain/src/container/App.js
@@ -29,26 +29,36 @@ class App extends Component {
     super();
     this.state = {
       input: "",
-      imageUrl: ""
+      imageUrl: "",
+      box: {}
     };
   }
 
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width;
+      topRow: clarifaiFace.top_row * height;
+      rightCol: width - (clarifaiFace.right_col * width );
+      bottomRow: heigth - (clarifaiFace.bottom_row * height)
+    };
+  };
+  
   onInputChange = event => {
     this.setState({ input: event.target.value });
   };
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function(response) {
-        console.log(
-          response.outputs[0].data.regions[0].region_info.bounding_box
-        );
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => this.calculateFaceLocation(response))
+      .catch(err => console.log(err));
   };
 
   render() {

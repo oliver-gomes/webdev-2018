4777b10fb45a43d43ee429beea4bd175ac6c75bf
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-09 00:30:56 -0400

    file moved

--- react-app/face-recognition-brain/src/container/App.js
+++ react-app/face-recognition-brain/src/container/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navigation from "../components/Navigation";
-import Logo from "../components/Logo";
+import Logo from "../components/Logo/Logo";
+import ImageLinkForm from "../components/ImageLinkForm/ImageLinkForm";
+import Rank from "../components/Rank/Rank";
 
 class App extends Component {
   render() {
@@ -9,8 +11,10 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
-        {/* 
+        <Rank />
         <ImageLinkForm />
+        {/* 
+        
         <FaceRecognition /> */}
       </div>
     );

69adfeda4ef1e67f44423c9926dc1d516e672a9a
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-09 18:08:49 -0400

    htmlFor implement

--- react-app/face-recognition-brain/src/components/Register/Register.js
+++ react-app/face-recognition-brain/src/components/Register/Register.js
@@ -4,11 +4,11 @@ const Register = ({ onRouteChange }) => {
   return (
     <article className="br3 ba dark-gray b--black-10 mv6 shadow-5 w-100 w-50-m w-25-l mw5 center">
       <main className="pa4 black-80">
-        <form className="measure ">
+        <div className="measure ">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f2 fw6 ph0 mh0">Register</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" for="email-address">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
                 Name
               </label>
               <input
@@ -20,7 +20,7 @@ const Register = ({ onRouteChange }) => {
             </div>
 
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" for="email-address">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
                 Email
               </label>
               <input
@@ -32,7 +32,7 @@ const Register = ({ onRouteChange }) => {
             </div>
 
             <div className="mv3">
-              <label className="db fw6 lh-copy f6" for="password">
+              <label className="db fw6 lh-copy f6" htmlFor="password">
                 Password
               </label>
               <input
@@ -51,7 +51,7 @@ const Register = ({ onRouteChange }) => {
               value="Register"
             />
           </div>
-        </form>
+        </div>
       </main>
     </article>
   );

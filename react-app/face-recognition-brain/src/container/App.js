7bb8bcf87d9016e56185ee3c16933b68b7ab9499
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-09 13:37:46 -0400

    Clarifai app added

--- react-app/face-recognition-brain/src/container/App.js
+++ react-app/face-recognition-brain/src/container/App.js
@@ -5,6 +5,11 @@ import Logo from "../components/Logo/Logo";
 import ImageLinkForm from "../components/ImageLinkForm/ImageLinkForm";
 import Rank from "../components/Rank/Rank";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
+
+const app = new Clarifai.App({
+  apiKey: "e053e4b7323a4712abaeacda8d197db7"
+});
 
 const particlesOptions = {
   particles: {
@@ -19,6 +24,33 @@ const particlesOptions = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
+
+  onInputChange = event => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    app.models
+      .predict(
+        "a403429f2ddf4b49b307e318f00e528b",
+        "https://samples.clarifai.com/face-det.jpg"
+      )
+      .then(
+        function(response) {
+          console.log(response);
+        },
+        function(err) {
+          // there was an error
+        }
+      );
+  };
+
   render() {
     return (
       <div className="App">
@@ -26,10 +58,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* 
-        
-        <FaceRecognition /> */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition />
       </div>
     );
   }

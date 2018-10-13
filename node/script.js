fb8f480d1f6d1aac3df8985c105f9d5671db6fda
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-12 20:29:36 -0400

    file system module

---
+++ node/script.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+// Read
+fs.readFile("./hello.txt", (err, data) => {
+  if (err) {
+    console.log("Errror!");
+  }
+  console.log(data.toString("utf8"));
+});
+
+// Append
+fs.appendFile("./hello.txt", "This is so cool", err => {
+  if (err) {
+    console.log(err);
+  }
+});
+
+// Write
+fs.writeFile("bye.txt", "Sad to see you go", err => {
+  if (err) {
+    console.log(err);
+  }
+});

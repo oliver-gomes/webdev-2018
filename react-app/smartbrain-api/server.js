afd7f8f4d000e27ebf93624cf13d0163477fd834
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-15 23:51:12 -0400

    signin Post request handling

--- react-app/smartbrain-api/server.js
+++ react-app/smartbrain-api/server.js
@@ -1,13 +1,44 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+};
 
 app.get("/", (req, res) => {
   res.send("this is working");
 });
 
 app.post("/signin", (req, res) => {
-  res.send("signing");
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
 });
+
 app.listen(3000, () => {
   console.log("app is running");
 });

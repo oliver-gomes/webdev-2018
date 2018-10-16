417ecee277e1112d9933f332282cea3f5687630c
Oliver-Gomes <ogomesphoto@gmail.com>
2018-10-16 02:08:02 -0400

    Get profile id setup

--- react-app/smartbrain-api/server.js
+++ react-app/smartbrain-api/server.js
@@ -25,7 +25,7 @@ const database = {
 };
 
 app.get("/", (req, res) => {
-  res.send("this is working");
+  res.send(database.users);
 });
 
 app.post("/signin", (req, res) => {
@@ -50,6 +50,17 @@ app.post("/register", (req, res) => {
     joined: new Date()
   });
   res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      res.json(user);
+    } else {
+      res.status(404).json("no such user");
+    }
+  });
 });
 
 app.listen(3000, () => {
